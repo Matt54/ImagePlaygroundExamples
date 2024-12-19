765581508279fb5cfae9bb6f7f4854f93b6a9098
Matt Pfeiffer <Mattpfeiffer54@gmail.com>
2024-12-18 18:38:21 -0600

    Initial Commit

---
+++ ImagePlaygroundExamples/ImagePlaygroundExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePlaygroundExamplesApp.swift
+//  ImagePlaygroundExamples
+//
+//  Created by Matt Pfeiffer on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ImagePlaygroundExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
